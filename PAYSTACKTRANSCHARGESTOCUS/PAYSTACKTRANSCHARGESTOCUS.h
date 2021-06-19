5cc9ae67fba2586c256dccb8bf158b4ab404c087
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2021-06-19 09:31:18 +0100

    Initial Commit

---
+++ PAYSTACKTRANSCHARGESTOCUS/PAYSTACKTRANSCHARGESTOCUS.h
@@ -0,0 +1,18 @@
+//
+//  PAYSTACKTRANSCHARGESTOCUS.h
+//  PAYSTACKTRANSCHARGESTOCUS
+//
+//  Created by Adewoye Kolade on 19/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PAYSTACKTRANSCHARGESTOCUS.
+FOUNDATION_EXPORT double PAYSTACKTRANSCHARGESTOCUSVersionNumber;
+
+//! Project version string for PAYSTACKTRANSCHARGESTOCUS.
+FOUNDATION_EXPORT const unsigned char PAYSTACKTRANSCHARGESTOCUSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PAYSTACKTRANSCHARGESTOCUS/PublicHeader.h>
+
+
