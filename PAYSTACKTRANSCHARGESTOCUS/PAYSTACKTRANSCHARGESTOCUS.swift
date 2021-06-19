db79f036cd346db5c6ac553706339596a2ad437b
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2021-06-19 12:22:20 +0100

    added the functions

---
+++ PAYSTACKTRANSCHARGESTOCUS/PAYSTACKTRANSCHARGESTOCUS.swift
@@ -0,0 +1,46 @@
+//
+//  PAYSTACKTRANSCHARGESTOCUS.swift
+//  PAYSTACKTRANSCHARGESTOCUS
+//
+//  Created by Adewoye Kolade on 19/06/2021.
+//
+
+import Foundation
+
+public final class PAYSTACKTRANSCHARGESTOCUS {
+
+    let name = "PAYSTACKTRANSCHARGESTOCUS"
+    private let DECIMAL_FEE = 0.0150;
+    private let FEE_CAP = 2000.00;
+    private let FLAT_FEE = 100.00;
+    
+    public func calculateTotalPrice(amount: String) -> String {
+        let amt:Double = Double(amount)!;
+        if(amt<100.0){
+            return String(Int(amt+1.0));
+        }else if(amt<2500){
+            return calculateFor2500Below(amount: amt);
+        }else{
+            return calculateFor2500Above(amount: amt);
+        }
+    }
+
+    private func calculateFor2500Below(amount: Double) -> String{
+        let applicableFees = DECIMAL_FEE * amount;
+        if(applicableFees>FEE_CAP){
+            return String(amount+FEE_CAP);
+        }else{
+            return String(Int(((amount)/(1-(DECIMAL_FEE)))+0.01));
+        }
+    }
+
+    private func calculateFor2500Above(amount: Double) -> String{
+            let applicableFees = (DECIMAL_FEE * amount)+FLAT_FEE;
+            if(applicableFees>FEE_CAP){
+                return String(amount+FEE_CAP);
+            }else{
+                return String(Int(((amount+FLAT_FEE)/(1-(DECIMAL_FEE)))+0.01));
+            }
+        }
+    
+}
--- PAYSTACKTRANSCHARGESTOCUSTests/PAYSTACKTRANSCHARGESTOCUSTests.swift
+++ PAYSTACKTRANSCHARGESTOCUSTests/PAYSTACKTRANSCHARGESTOCUSTests.swift
@@ -9,25 +9,15 @@ import XCTest
 @testable import PAYSTACKTRANSCHARGESTOCUS
 
 class PAYSTACKTRANSCHARGESTOCUSTests: XCTestCase {
+    
+    var paystackTransChargesToCus: PAYSTACKTRANSCHARGESTOCUS!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        override func setUp() {
+            paystackTransChargesToCus = PAYSTACKTRANSCHARGESTOCUS()
         }
-    }
+
+        func testCalculateTotalPrice() {
+            XCTAssertEqual(paystackTransChargesToCus.calculateTotalPrice(amount: "100"), "101")
+        }
 
 }
