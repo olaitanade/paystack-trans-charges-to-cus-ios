b9d03fed1cc4dae0635954834857876ba0355c79
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2021-06-19 10:06:19 +0100

    project setup

---
+++ PaystackTransChargesToCusExamples/PaystackTransChargesToCusExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaystackTransChargesToCusExamplesApp.swift
+//  PaystackTransChargesToCusExamples
+//
+//  Created by Adewoye Kolade on 19/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PaystackTransChargesToCusExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
